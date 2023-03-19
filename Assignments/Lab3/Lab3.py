3d3bc99f1cfeb139af71d175f77717dc645fb9e2
Joshua <jtgenova@cougarnet.uh.edu>
2023-03-19 13:49:19 -0500

    fixed prelab

--- Assignments/Lab3/Lab3.py
+++ Assignments/Lab3/Lab3.py
@@ -94,73 +94,57 @@ if __name__=="__main__":
     bx = 92.000
    
     # Image 1
-    delta_X = -122.01704301790505
-    delta_Y = 123.53429666924897
-    A = 0.011899426266928175
-    B = 0.000000299767744395384
-    C = -0.00000134050132901044
-    D = 0.011901264695956251
-    A_mat = np.array([[A, B], [C, D]])
+    delta_X1 = -122.01704301790505
+    delta_Y1 = 123.53429666924897
+    A1 = 0.011899426266928175
+    B1 = 0.000000299767744395384
+    C1 = -0.00000134050132901044
+    D1 = 0.011901264695956251
+    A_mat1 = np.array([[A1, B1], [C1, D1]])
 
     xc1 = [9460, 17400, 10059, 19158, 11844, 17842]
     yc1 = [-2292, -1661, -10883, -10412, -17253, -18028]
 
+    # Image 2
+    delta_X2 = -122.19211044565897
+    delta_Y2 = 123.51804729053579
+    A2 = 0.011900088285313318
+    B2 = -8.456447779614914e-06
+    C2 = 7.403491422692827e-06
+    D2 = 0.011901033060072988
+    A_mat2 = np.array([[A2, B2], [C2, D2]])
+
+    xc2 = [1411, 9416, 2275, 11129, 4160, 10137]
+    yc2 = [-2081, -1167, -10787, -10048, -17085, -17690]
+
     idx = 0
-    correction = np.zeros(shape=(len(xc1),2))
-    fid_coords = np.zeros(shape=(len(xc1),2))
+    correction_left = np.zeros(shape=(len(xc1),2))
+    correction_right = np.zeros(shape=(len(xc2),2))
     for i in range(len(xc1)):
-        # get fiducial coordinates
-        xf, yf = pre_lab_3.get_fiducial(xc1[i], yc1[i], A_mat, delta_X, delta_Y)
-        fid_coords[idx][0] = xf
-        fid_coords[idx][1] = yf
+        # get fiducial coordinates 1
+        xf, yf = pre_lab_3.get_fiducial(xc1[i], yc1[i], A_mat1, delta_X1, delta_Y1)
+        # correct images 1
+        xl, yl = pre_lab_3.get_total(xf, yf, principal_point_offset, radial_lens_distortion, decentering_distortion, focal_length, k_atmos)
+        correction_left[idx][0] = round(xl, 3)
+        correction_left[idx][1] = round(yl, 3)
 
-        x_total, y_total = pre_lab_3.get_total(xf, yf, principal_point_offset, radial_lens_distortion, decentering_distortion, focal_length, k_atmos)
-        correction[idx][0] = x_total
-        correction[idx][1] = y_total
+        # get fiducial coordinates 2
+        xf, yf = pre_lab_3.get_fiducial(xc2[i], yc2[i], A_mat2, delta_X2, delta_Y2)
+        # correct images 2
+        xr, yr = pre_lab_3.get_total(xf, yf, principal_point_offset, radial_lens_distortion, decentering_distortion, focal_length, k_atmos)
+        correction_right[idx][0] = round(xr, 3)
+        correction_right[idx][1] = round(yr, 3)
         idx += 1
-    # print(f'Total Correction: \n {correction}\n')
-    xf = correction[:,0]
-    yf = correction[:,1]
+    print(f'Total Correction Left: \n {correction_left}\n')
+    print(f'Total Correction Right: \n {correction_right}\n')
     idx = 0
 
-    A_matrix = np.zeros(shape=(len(xf),5))
-    for i in range(len(xf)):
-        dby, dbz, dw, dphi, dkappa = find_A_elems(0, 0, c, xf[i], yf[i], c, bx)
-        A_matrix[idx][0] = dby
-        A_matrix[idx][1] = dbz
-        A_matrix[idx][2] = dw
-        A_matrix[idx][3] = dphi
-        A_matrix[idx][4] = dkappa
-    print(f'A matrix = {A_matrix}')
-
-    
-
-
-
-##############################################################################################################################################################
-    # # Image 2
-    # delta_X = -122.19211044565897
-    # delta_Y = 123.51804729053579
-    # A = 0.011900088285313318
-    # B = -8.456447779614914e-06
-    # C = 7.403491422692827e-06
-    # D = 0.011901033060072988
-    # A_mat = np.array([[A, B], [C, D]])
-
-    # xc2 = [1411, 9416, 2275, 11129, 4160, 10137]
-    # yc2 = [-2081, -1167, -10787, -10048, -17085, -17690]
-
-    # idx = 0
-    # correction = np.zeros(shape=(len(xc2),2))
-    # fid_coords = np.zeros(shape=(len(xc2),2))
-    # for i in range(len(xc2)):
-    #     # get fiducial coordinates
-    #     xf, yf = pre_lab_3.get_fiducial(xc2[i], yc2[i], A_mat, delta_X, delta_Y)
-    #     fid_coords[idx][0] = xf
-    #     fid_coords[idx][1] = yf
-
-    #     x_total, y_total = pre_lab_3.get_total(xf, yf, principal_point_offset, radial_lens_distortion, decentering_distortion, focal_length, k_atmos)
-    #     correction[idx][0] = x_total
-    #     correction[idx][1] = y_total
-    #     idx += 1
-    # print(f'Total Correction: \n {correction}\n')
+    # A_matrix = np.zeros(shape=(len(xf),5))
+    # for i in range(len(xf)):
+    #     dby, dbz, dw, dphi, dkappa = find_A_elems(0, 0, c, xl[i], yl[i], c, bx)
+    #     A_matrix[idx][0] = dby
+    #     A_matrix[idx][1] = dbz
+    #     A_matrix[idx][2] = dw
+    #     A_matrix[idx][3] = dphi
+    #     A_matrix[idx][4] = dkappa
+    # print(f'A matrix = {A_matrix}')
